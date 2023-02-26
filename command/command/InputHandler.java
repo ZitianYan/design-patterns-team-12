7b5ac4f42a61cd232a20fcba5fffa5751146a951
Peyton <pmfisher@email.sc.edum>
2023-02-26 18:00:54 -0500

    Command design program

--- command/command/InputHandler.java
+++ command/command/InputHandler.java
@@ -2,17 +2,36 @@ package command;
 
 import java.util.HashMap;
 
-public class InputHandler {
-    HashMap<String, Command> commands = new HashMap<>();
-
-    public InputHandler(StopWatch watch){
-        
-       
+public class InputHandler 
+{
+	/*
+	 * Class that creates an instance of the stop watch and uses hash
+	 * map functions to execute either a count down from one minute,
+	 * five minutes, or thirty seconds by Peyton Fisher
+	 */
+    private HashMap<String, Command> commands;
+    private StopWatch watch;
+    
+    public InputHandler(StopWatch watch)
+    {
+        this.watch = watch;
+        commands = new HashMap<String, Command>();
+        commands.put("1", new OneMinCommand(watch));
+        commands.put("5", new FiveMinCommand(watch));
+        commands.put("30", new ThirtySecCommand(watch));
     }
 
-    public inputeEntered(String data){
-
-        
+    public boolean inputEntered(String data)
+    {
+    	Command command = commands.get(data);
+    	if(command != null)
+    	{
+    		command.execute();
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	} 
     }
-
 }
